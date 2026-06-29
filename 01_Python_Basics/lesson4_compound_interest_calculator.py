54cb5d0b55e2fcb4ff4818349cf8db7b8b64eb91
oweynlee <oweyn.business@gmail.com>
2026-06-29 22:13:27 +0800

    Complete lesson 4: Functions

--- 01_Python_Basics/lesson4_compound_interest_calculator.py
+++ 01_Python_Basics/lesson4_compound_interest_calculator.py
@@ -6,7 +6,7 @@ def compound_interest(principal, rate, years):
 
     for _ in range(1,years+1):
         principal = principal*(1+rate/100)
-    return (principal)
+    return principal
 
 result = compound_interest(principal, rate, years)
 print(f"Final Amount: RM {result:.2f}")
