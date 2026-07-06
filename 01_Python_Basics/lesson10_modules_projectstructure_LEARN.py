b6e5ed5dc2bea5285109805ef6e52dce8a2afd5d
oweynlee <oweyn.business@gmail.com>
2026-07-06 15:43:30 +0800

    Complete Lesson 10 (Part1) : Modules & Project Structures

---
+++ 01_Python_Basics/lesson10_modules_projectstructure_LEARN.py
@@ -0,0 +1,6 @@
+from tracker import ExpenseTracker  # From a module (.py) import its function/class
+
+tracker = ExpenseTracker()  #set tracker as an account
+tracker.function
+
+# These uses to handle large number classified account, without any correlations
